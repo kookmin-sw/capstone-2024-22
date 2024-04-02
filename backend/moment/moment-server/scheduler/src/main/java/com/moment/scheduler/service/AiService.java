2befe6dac7d37767b967d586a2fd21def43d1648
jykim-rust <alexj99@naver.com>
2024-04-02 14:42:18 +0900

    :sparkles: file_name과 file_path로 인자 수정

--- backend/moment/moment-server/scheduler/src/main/java/com/moment/scheduler/service/AiService.java
+++ backend/moment/moment-server/scheduler/src/main/java/com/moment/scheduler/service/AiService.java
@@ -1,6 +1,7 @@
 package com.moment.scheduler.service;
 
 import com.moment.scheduler.client.AiClient;
+import com.moment.scheduler.dto.request.AiModelRunRequestDTO;
 import com.moment.scheduler.dto.response.AiModelRunResponseDTO;
 import com.moment.scheduler.dto.response.EmotionResponseDTO;
 import lombok.AllArgsConstructor;
@@ -10,8 +11,14 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class AiService {
     private final AiClient aiClient;
+    private final static String file_path = "";
 
     public AiModelRunResponseDTO.RunModel runAi(String fileName) {
-        return aiClient.runAi(fileName);
+        return aiClient.runAi(
+                AiModelRunRequestDTO.RunModel.builder()
+                        .file_name(fileName)
+                        .file_path(file_path)
+                        .build()
+        );
     }
 }
