e206ffc7aedeb2d96e90e44fba97ac219311c3b9
sikwan <yysskk99999@naver.com>
2024-04-05 01:52:57 +0900

    Onboarding view add

--- iOS/moment-iOS/Onboarding/OnboardingView.swift
+++ iOS/moment-iOS/Onboarding/OnboardingView.swift
@@ -17,13 +17,40 @@ struct OnboardingView: View {
     @StateObject var audioRecorderManager = AudioRecorderManager()
     @State private var showingCustomAlert = false
     @StateObject private var cardviewModel = CardViewModel()
+    //@StateObject private var viewModel = OnboardingViewModel()
+        @State private var currentPage = 0
     
     var body: some View {
         
        
         NavigationStack(path : $pathModel.paths){
-              OnboardingContentView(onboardingViewModel: onboardingViewModel)
-         
+             // OnboardingContentView(onboardingViewModel: onboardingViewModel)
+            VStack {
+                      TabView(selection: $currentPage) {
+                          ForEach(0..<onboardingViewModel.onboardingContents.count, id: \.self) { index in
+                              VStack {
+                              
+                                  Image(onboardingViewModel.onboardingContents[index].imageFileName)
+                                      .resizable()
+                                      .scaledToFit()
+                                      
+                                
+                              }
+                              .tag(index)
+                          }
+                      }
+                   
+                      
+                
+                      
+                HStack{
+                  Spacer()
+                    StartBtnView()
+                        .padding()
+                }
+                Spacer()
+                
+                  }
           
               
                 .navigationDestination(for: PathType.self, destination: { // 여기서 for 부분에서 PathType 부분에서 해당 네비게이션을 정의를 하는부분인거임
@@ -90,101 +117,6 @@ struct OnboardingView: View {
     }
 }
 
-
-
-// MARK: - 온보딩 컨텐츠 뷰
-private struct OnboardingContentView: View {
-    
-    @ObservedObject private var onboardingViewModel : OnboardingViewModel
-    
-    fileprivate init(onboardingViewModel: OnboardingViewModel) {
-        self.onboardingViewModel = onboardingViewModel
-    }
-    
-    fileprivate var body: some View{
-        VStack{
-            //온보딩 셀리스트뷰
-            OnboardingCellListView(onboardingViewModel: onboardingViewModel)
-            // 시작 버튼뷰
-            Spacer()
-            StartBtnView()
-        }
-        .edgesIgnoringSafeArea(.top)
-    }
-}
-//MARK : - 온보딩 셀 리스트 뷰
-private struct OnboardingCellListView : View{
-    @ObservedObject private var onboardingViewModel : OnboardingViewModel
-    @State private var selectedIndex : Int
-    
-    fileprivate
-    init(onboardingViewModel: OnboardingViewModel,
-        selectedIndex: Int = 0
-    ) {
-        self.onboardingViewModel = onboardingViewModel
-        self.selectedIndex = selectedIndex
-    }
-    
-    fileprivate var body: some View{
-        TabView(selection: $selectedIndex){
-            ForEach(Array(onboardingViewModel.onboardingContents.enumerated()),id : \.element){
-               index, onboardingContent in
-                OnboardingCellView(onboardingContent: onboardingContent)
-                    .tag(index)
-            }
-        }
-        .tabViewStyle(.page(indexDisplayMode: .never))
-        .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height / 1.5)
-        .background(
-        selectedIndex % 2 == 0
-        ? Color.customSky
-        : Color.customCoolGray
-        
-        )
-        .clipped()
-    }
-}
-
-//MARK : - 온보딩 셀 뷰
-private struct OnboardingCellView : View{
-    private var onboardingContent: OnboardingContent
-    
-    fileprivate init(onboardingContent: OnboardingContent){
-        self.onboardingContent = onboardingContent
-    }
-    
-    fileprivate var body: some View{
-        VStack{
-            Image(onboardingContent.imageFileName)
-                .resizable()
-                .scaledToFit()
-            
-            
-            HStack{
-                Spacer()
-                VStack{
-                    Spacer()
-                        .frame(height: 46)
-                    
-                    Text(onboardingContent.title)
-                        .font(.system(size: 16,weight: .bold))
-                    
-                    Spacer()
-                        .frame(height: 5)
-                    
-                    Text(onboardingContent.subTitle)
-                        .font(.system(size: 16))
-                }
-                
-                Spacer()
-            }
-            .background(Color.customWhite)
-            .cornerRadius(0)
-        }
-        .shadow(radius: 10)
-    }
-}
-
 //시작하기 버튼
 private struct StartBtnView : View{
     @EnvironmentObject private var pathModel : PathModel
@@ -197,12 +129,12 @@ private struct StartBtnView : View{
             label :{
                 HStack{
                     Text("시작하기")
-                        .font(.system(size: 16,weight : .medium))
-                        .foregroundColor(.customBackgroundGreen)
+                        .font(.pretendardMedium14)
+                        .foregroundColor(.black)
                     
-                    Image("arrow")
-                        .renderingMode(.template)
-                        .foregroundColor(.green)
+                    Image("arrowST")
+                        
+                    
                 }
             }
         )
