ccfdd7666b8b0681a6fd733e11deb41decab0a5e
penguinseohyun <tjgus1036@naver.com>
2024-04-05 11:17:04 +0900

    [HSH] - feat 인증번호 유효시간초 기능 구현 - res 알람 이미지 2종류 추가

--- Android/moment-android/app/src/main/java/com/capstone/android/application/ui/theme/MomentText_hsh.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/ui/theme/MomentText_hsh.kt
@@ -1,17 +1,13 @@
 package com.capstone.android.application.ui.theme
 
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
@@ -111,6 +107,22 @@ fun HintText(content : String){
             fontFamily = PretendardFamily,
             fontWeight = FontWeight.Medium,
             fontSize = 18.sp)
+    )
+}
+
+@Composable
+fun CountText(timeLeft: Long){
+
+    val min = (timeLeft / 1000) / 60
+    val sec = (timeLeft / 1000) % 60
+
+    Text(
+        text = String.format("%02d:%02d", min, sec),
+        style = TextStyle(
+            color = neutral_600,
+            fontFamily = PretendardFamily,
+            fontWeight = FontWeight.Medium,
+            fontSize = 11.sp)
     )
 }
 
