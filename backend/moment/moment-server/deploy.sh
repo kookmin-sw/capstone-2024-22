407b1f8525c77cf0e47df1d7bb6d3727bbbd8e70
j-yong99 <alexj99@naver.com>
2024-04-06 22:18:16 +0900

    :sparkles: CI/CD 설정

--- backend/moment/moment-server/deploy.sh
+++ backend/moment/moment-server/deploy.sh
@@ -1,4 +1,4 @@
-ssh -tt -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -o StrictHostKeyChecking=no << 'ENDSSH'
+ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -o StrictHostKeyChecking=no << 'ENDSSH'
 
 cd capstone-2024-22/backend/moment/moment-server
 git pull origin "deploy/v1"
