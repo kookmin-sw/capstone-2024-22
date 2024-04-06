85138ffa605d057df7a2ccc047c1b4617b77059b
j-yong99 <alexj99@naver.com>
2024-04-06 22:00:12 +0900

    :sparkles: CI/CD 설정

--- backend/moment/moment-server/deploy.sh
+++ backend/moment/moment-server/deploy.sh
@@ -1,6 +1,6 @@
 ssh -tt -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -o StrictHostKeyChecking=no << 'ENDSSH'
 
-cd capstone-2024-22
+cd capstone-2024-22/backend/moment/moment-server
 git pull origin "deploy/v1"
 docker-compose up --build -d
 ENDSSH
