e371508e3877e960866c724f74319104cc4e9639
j-yong99 <alexj99@naver.com>
2024-04-07 11:34:09 +0900

    :sparkles: CI/CD 설정

--- backend/moment/moment-server/deploy.sh
+++ backend/moment/moment-server/deploy.sh
@@ -1,15 +1,5 @@
 ssh -tt -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -o StrictHostKeyChecking=no << 'ENDSSH'
 cd capstone-2024-22/backend/moment/moment-server
-# Check if the container is already running
-if [ "$(docker ps -q -f name=moment-server)" ]; then
-    # If the container is running, stop and remove it
-    docker stop moment-server
-    docker rm moment-server
-fi
-
-# Pull the latest changes from the repository
-git pull origin "deploy/v1"
-
-# Build and start the container
+docker-compose down
 docker-compose up --build -d
 ENDSSH
