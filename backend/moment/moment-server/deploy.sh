5bb099f31ce7f2921d81dad28a84d2baa541c08e
j-yong99 <alexj99@naver.com>
2024-04-07 11:44:33 +0900

    :sparkles: boolean값 처리 대신 String으로 변경

--- backend/moment/moment-server/deploy.sh
+++ backend/moment/moment-server/deploy.sh
@@ -1,5 +1,6 @@
 ssh -tt -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -o StrictHostKeyChecking=no << 'ENDSSH'
 cd capstone-2024-22/backend/moment/moment-server
+git pull
 docker-compose down
 docker-compose up --build -d
 ENDSSH
