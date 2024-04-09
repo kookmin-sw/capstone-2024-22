99c9d71352d4cbfdf4755e5f369c3d93e3109ea8
kmj-99 <nex1223@naver.com>
2024-04-09 16:52:15 +0900

    module : 네트워크 관련 테스트 모듈 추가

--- Android/moment-android/app/src/main/java/com/capstone/android/application/data/remote/TestRemote.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/data/remote/TestRemote.kt
@@ -1,4 +1,20 @@
 package com.capstone.android.application.data.remote
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
 class TestRemote {
+
+    val userMessage :Flow<List<String>> = flow{
+        while(true){
+            val testArray = ArrayList<String>()
+            testArray.add("one")
+            testArray.add("two")
+            emit(testArray)
+            delay(100)
+        }
+
+    }
+
 }
--- Android/moment-android/app/src/main/java/com/capstone/android/application/presentation/TestViewModel.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/presentation/TestViewModel.kt
@@ -1,4 +1,11 @@
 package com.capstone.android.application.presentation
 
-class TestViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.channels.*
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class TestViewModel: ViewModel() {
+//    private val events = Channel<Lifecycle.Event>()
+//
+//    private val _state = MutableStateFlow(Lifecycle.State())
 }
