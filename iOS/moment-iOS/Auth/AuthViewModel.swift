3ccd938314c3886c20db1c08d3498d634011b8ff
sikwan <yysskk99999@naver.com>
2024-04-13 00:54:16 +0900

    authnumber view network add success

--- iOS/moment-iOS/Auth/AuthViewModel.swift
+++ iOS/moment-iOS/Auth/AuthViewModel.swift
@@ -13,32 +13,57 @@ import Alamofire
 class AuthViewModel: ObservableObject {
     @Published var email: String = ""
     // 필요한 경우 추가적인 상태 변수를 선언
-
+    @Published var verificationCode: String = ""
+    @Published var isCodeValid: Bool? = nil
+    
     func requestAuthCode() {
         let parameters: [String: Any] = [
             "email": email,
             "isSignUp": "true"
         ]
-
+        
         AF.request("http://wasuphj.synology.me:8000/auth/code",
                    method: .post,
                    parameters: parameters,
                    encoding: JSONEncoding.default)
-            .validate() // 상태 코드 200..<300 범위를 검증
-            .responseDecodable(of: AuthResponse.self) { response in
-                switch response.result {
-                case .success(let authResponse):
-                    DispatchQueue.main.async {
-                        // 성공 시 처리
-                        print("AccessToken: \(authResponse.data.accessToken)")
-                        // 상태 업데이트 로직
-                    }
-                case .failure(let error):
-                    // 실패 시 처리
-                    print("Error: \(error)")
+        .validate() // 상태 코드 200..<300 범위를 검증
+        .responseDecodable(of: AuthResponse.self) { response in
+            switch response.result {
+            case .success(let authResponse):
+                DispatchQueue.main.async {
+                    // 성공 시 처리
+                    print("AccessToken: \(authResponse.data.accessToken)")
+                    // 상태 업데이트 로직
                 }
+            case .failure(let error):
+                // 실패 시 처리
+                print("Error: \(error)")
             }
+        }
     }
+    
+    func verifyCode(completion: @escaping (Bool) -> Void) {
+           let parameters: [String: String] = [
+               "code": verificationCode
+           ]
+           
+           AF.request("http://wasuphj.synology.me:8000/auth/verify", method: .patch, parameters: parameters, encoding: JSONEncoding.default)
+               .validate()
+               .responseJSON { response in
+                   switch response.result {
+                   case .success:
+                       DispatchQueue.main.async {
+                           self.isCodeValid = true
+                           completion(true)
+                       }
+                   case .failure:
+                       DispatchQueue.main.async {
+                           self.isCodeValid = false
+                           completion(false)
+                       }
+                   }
+               }
+       }
 }
 
 // 서버 응답을 위한 구조체
