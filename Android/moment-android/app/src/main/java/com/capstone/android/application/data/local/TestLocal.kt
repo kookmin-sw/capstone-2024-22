3736fbcd6e0da7d7d72cbf26f9999ba9cfc0d314
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-04-16 16:05:24 +0900

    add : 로컬 DB 테스트 모듈 추가

--- Android/moment-android/app/src/main/java/com/capstone/android/application/data/local/TestLocal.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/data/local/TestLocal.kt
@@ -1,4 +1,18 @@
 package com.capstone.android.application.data.local
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 class TestLocal {
+    private fun addUser(){
+//        db = MomentRecordDatabase.getInstance(context = applicationContext)!!
+        var name = "name"
+        var age = "1"
+        var phone = "214124"
+
+        CoroutineScope(Dispatchers.IO).launch {
+//            db.recordDao().insert(MomentRecord(name = name, age = age, phone = phone))
+        }
+    }
 }
