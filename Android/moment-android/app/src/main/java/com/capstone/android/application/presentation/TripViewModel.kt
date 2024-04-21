4294ecb5069541cb4e4212258b57fe0d06b25cab
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-04-21 15:58:52 +0900

    refector : ViewModel에 여행 도메인 API를 UI에서 쓸 수 있도록 함수 구성

--- Android/moment-android/app/src/main/java/com/capstone/android/application/presentation/TripViewModel.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/presentation/TripViewModel.kt
@@ -1,12 +1,14 @@
 package com.capstone.android.application.presentation
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.capstone.android.application.data.remote.trip.TripRepository
+import com.capstone.android.application.data.remote.trip.model.trip_all.GetTripAllResponse
 import com.capstone.android.application.data.remote.trip.model.trip_patch.request.PostTripPatchRequest
 import com.capstone.android.application.data.remote.trip.model.trip_register.request.PostTripRegisterRequest
+import com.capstone.android.application.domain.response.ApiResponse
+import com.capstone.android.application.domain.response.MomentResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
@@ -15,16 +17,60 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TripViewModel @Inject constructor(private val tripRepository:TripRepository): ViewModel() {
-    val tripAll : MutableLiveData<String> by lazy {
-        MutableLiveData<String>()
+
+    // 여행목록 조회 성공
+    val getTripAllSuccess : MutableLiveData<GetTripAllResponse> by lazy{
+        MutableLiveData<GetTripAllResponse>()
     }
+
+    // 여행목록 조회 실패
+    val getTripAllFailure : MutableLiveData<ApiResponse.Error<Exception>> by lazy {
+        MutableLiveData<ApiResponse.Error<Exception>>()
+    }
+
+    // 여행등록 성공
+    val postTripRegisterSuccess : MutableLiveData<MomentResponse> by lazy {
+        MutableLiveData<MomentResponse>()
+    }
+
+    // 여행등록 실패
+    val postTripRegisterFailure : MutableLiveData<ApiResponse.Error<Exception>> by lazy {
+        MutableLiveData<ApiResponse.Error<Exception>>()
+    }
+
+    // 여행삭제 성공
+    val deleteTripSuccess : MutableLiveData<MomentResponse> by lazy {
+        MutableLiveData<MomentResponse>()
+    }
+
+    // 여행삭제 실패
+    val deleteTripFailure : MutableLiveData<ApiResponse.Error<Exception>> by lazy {
+        MutableLiveData<ApiResponse.Error<Exception>>()
+    }
+
+    // 여행수정 성공
+    val patchTripSuccess : MutableLiveData<MomentResponse> by lazy {
+        MutableLiveData<MomentResponse>()
+    }
+
+    // 여행수정 실패
+    val patchTripFailure:MutableLiveData<ApiResponse.Error<Exception>> by lazy {
+        MutableLiveData<ApiResponse.Error<Exception>>()
+    }
+
+
     fun getTripAll(){
         viewModelScope.launch {
             try {
-                val data = tripRepository.getTripAll()
-                tripAll.postValue(data.message())
+                val response = tripRepository.getTripAll()
+                if(response is ApiResponse.Success){
+                    getTripAllSuccess.postValue(response.data)
+                }else{
+
+                }
 
             } catch (e: HttpException) {
+                getTripAllFailure.postValue(ApiResponse.Error(e))
                 // Handle specific HTTP error codes
                 when (e.code()) {
                     404 -> {
@@ -33,9 +79,13 @@ class TripViewModel @Inject constructor(private val tripRepository:TripRepositor
                     // Handle other error codes
                 }
             } catch (e: IOException) {
+                getTripAllFailure.postValue(ApiResponse.Error(e))
+
                 // Handle network-related errors
 //                throw NetworkException("Network error occurred", e)
             } catch (e: Exception) {
+                getTripAllFailure.postValue(ApiResponse.Error(e))
+
                 // Handle other generic exceptions
             }
         }
@@ -47,14 +97,19 @@ class TripViewModel @Inject constructor(private val tripRepository:TripRepositor
         viewModelScope.launch {
             try {
 
-                val data = tripRepository.postTripRegister(
+                val response = tripRepository.postTripRegister(
                     body = body
                 )
-                Log.d("ewageawgewagewa",data.message())
-                tripAll.postValue(data.message())
+
+                if(response is ApiResponse.Success){
+                    postTripRegisterSuccess.postValue(response.data)
+                }else{
+
+                }
 
 
             } catch (e:HttpException){
+                postTripRegisterFailure.postValue(ApiResponse.Error(e))
                 when(e.code()){
                     404 -> {
 
@@ -62,8 +117,10 @@ class TripViewModel @Inject constructor(private val tripRepository:TripRepositor
                 }
 
             } catch (e:IOException){
+                postTripRegisterFailure.postValue(ApiResponse.Error(e))
 
             } catch (e:Exception){
+                postTripRegisterFailure.postValue(ApiResponse.Error(e))
 
             }
 
@@ -76,12 +133,19 @@ class TripViewModel @Inject constructor(private val tripRepository:TripRepositor
         viewModelScope.launch {
             try {
 
-                val data = tripRepository.deleteTrip(
+                val response = tripRepository.deleteTrip(
                     userId = userId
                 )
 
+                if(response is ApiResponse.Success){
+                    deleteTripSuccess.postValue(response.data)
+                }else{
+
+                }
+
 
             } catch (e:HttpException){
+                deleteTripFailure.postValue(ApiResponse.Error(e))
                 when(e.code()){
                     404 -> {
 
@@ -89,8 +153,11 @@ class TripViewModel @Inject constructor(private val tripRepository:TripRepositor
                 }
 
             } catch (e:IOException){
+                deleteTripFailure.postValue(ApiResponse.Error(e))
+
 
             } catch (e:Exception){
+                deleteTripFailure.postValue(ApiResponse.Error(e))
 
             }
 
@@ -104,13 +171,19 @@ class TripViewModel @Inject constructor(private val tripRepository:TripRepositor
         viewModelScope.launch {
             try {
 
-                val data = tripRepository.patchTrip(
+                val response = tripRepository.patchTrip(
                     userId = userId,
                     body = body
                 )
+                if(response is ApiResponse.Success){
+                    patchTripSuccess.postValue(response.data)
+                }else{
+
+                }
 
 
             } catch (e:HttpException){
+                patchTripFailure.postValue(ApiResponse.Error(e))
                 when(e.code()){
                     404 -> {
 
@@ -118,9 +191,9 @@ class TripViewModel @Inject constructor(private val tripRepository:TripRepositor
                 }
 
             } catch (e:IOException){
-
+                patchTripFailure.postValue(ApiResponse.Error(e))
             } catch (e:Exception){
-
+                patchTripFailure.postValue(ApiResponse.Error(e))
             }
 
         }
