cf2c04a1ef829faf20c6cbd50823e2e7864ad341
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-04-25 22:06:54 +0900

    chore : 함수 이름 수정

--- Android/moment-android/app/src/main/java/com/capstone/android/application/data/remote/trip/TripRepositoryInterface.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/data/remote/trip/TripRepositoryInterface.kt
@@ -1,11 +1,10 @@
 package com.capstone.android.application.data.remote.trip
 
 import com.capstone.android.application.data.remote.trip.model.trip_all.GetTripAllResponse
-import com.capstone.android.application.data.remote.trip.model.trip_patch.request.PostTripPatchRequest
+import com.capstone.android.application.data.remote.trip.model.trip_put.request.PutTripRequest
 import com.capstone.android.application.data.remote.trip.model.trip_register.request.PostTripRegisterRequest
 import com.capstone.android.application.domain.response.ApiResponse
 import com.capstone.android.application.domain.response.MomentResponse
-import retrofit2.Response
 
 interface TripRepositoryInterface {
     suspend fun getTripAll() : ApiResponse<GetTripAllResponse>
@@ -17,8 +16,7 @@ interface TripRepositoryInterface {
             userId:Int
     ) : ApiResponse<MomentResponse>
 
-    suspend fun patchTrip(
-        userId : Int,
-        body : PostTripPatchRequest
+    suspend fun putTrip(
+        body : PutTripRequest
     ) : ApiResponse<MomentResponse>
 }
