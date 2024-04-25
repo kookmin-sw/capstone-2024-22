60a183a583b3c608f021fa235cd941b67a6d751a
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-04-25 22:06:58 +0900

    chore : 함수 이름 수정

--- Android/moment-android/app/src/main/java/com/capstone/android/application/data/remote/trip/TripRetrofitInterface.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/data/remote/trip/TripRetrofitInterface.kt
@@ -1,18 +1,16 @@
 package com.capstone.android.application.data.remote.trip
 
 import com.capstone.android.application.data.remote.trip.model.trip_all.GetTripAllResponse
-import com.capstone.android.application.data.remote.trip.model.trip_patch.request.PostTripPatchRequest
+import com.capstone.android.application.data.remote.trip.model.trip_put.request.PutTripRequest
 import com.capstone.android.application.data.remote.trip.model.trip_register.request.PostTripRegisterRequest
 import com.capstone.android.application.domain.response.ApiResponse
 import com.capstone.android.application.domain.response.MomentResponse
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface TripRetrofitInterface {
     @GET("core/trip/all")
@@ -28,9 +26,8 @@ interface TripRetrofitInterface {
         @Path(value = "tripId") tripId : Int
     ) : ApiResponse<MomentResponse>
 
-    @PATCH("/core/trip")
-    suspend fun patchTripPatch(
-        @Query("userId") userId: Int,
-        @Body body : PostTripPatchRequest
+    @PUT("/core/trip")
+    suspend fun putTripPatch(
+        @Body body : PutTripRequest
     ) : ApiResponse<MomentResponse>
 }
