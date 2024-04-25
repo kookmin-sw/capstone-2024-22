637f9d7960a05e8a068647dae17be4e989e1a9b8
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-04-25 22:06:50 +0900

    chore : 함수 이름 수정

--- Android/moment-android/app/src/main/java/com/capstone/android/application/data/remote/trip/TripRepository.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/data/remote/trip/TripRepository.kt
@@ -1,11 +1,10 @@
 package com.capstone.android.application.data.remote.trip
 
 import com.capstone.android.application.data.remote.trip.model.trip_all.GetTripAllResponse
-import com.capstone.android.application.data.remote.trip.model.trip_patch.request.PostTripPatchRequest
+import com.capstone.android.application.data.remote.trip.model.trip_put.request.PutTripRequest
 import com.capstone.android.application.data.remote.trip.model.trip_register.request.PostTripRegisterRequest
 import com.capstone.android.application.domain.response.ApiResponse
 import com.capstone.android.application.domain.response.MomentResponse
-import retrofit2.Response
 import javax.inject.Inject
 
 class TripRepository @Inject constructor(private val tripRetrofitInterface:TripRetrofitInterface):TripRepositoryInterface {
@@ -27,10 +26,9 @@ class TripRepository @Inject constructor(private val tripRetrofitInterface:TripR
         return tripRetrofitInterface.deleteTripDelete(tripId = tripId)
     }
 
-    override suspend fun patchTrip(
-        userId :Int,
-        body: PostTripPatchRequest
+    override suspend fun putTrip(
+        body: PutTripRequest
     ): ApiResponse<MomentResponse> {
-        return tripRetrofitInterface.patchTripPatch(userId = userId, body = body)
+        return tripRetrofitInterface.putTripPatch( body = body)
     }
 }
