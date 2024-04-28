3adbc9222f414142beb98af7f8417fcfee0d54a0
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-04-28 17:18:33 +0900

    add :  CardViewModel에 카드 업로드 API를 UI에서 쓸 수 있도록 함수 추가

--- Android/moment-android/app/src/main/java/com/capstone/android/application/presentation/CardViewModel.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/presentation/CardViewModel.kt
@@ -1,18 +1,62 @@
 package com.capstone.android.application.presentation
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.capstone.android.application.data.remote.card.CardRepository
 import com.capstone.android.application.data.remote.card.model.card_modify.request.PutCardModifyRequest
+import com.capstone.android.application.data.remote.card.model.card_post.response.PostCardUploadResponse
+import com.capstone.android.application.domain.response.ApiResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
 class CardViewModel @Inject constructor(private val cardRepository: CardRepository): ViewModel() {
+
+    // 카드업로드 성공
+    val postCardUploadSuccess : MutableLiveData<PostCardUploadResponse> by lazy{
+        MutableLiveData<PostCardUploadResponse>()
+    }
+
+    // 카드업로드 실패
+    val postCardUploadFailure : MutableLiveData<ApiResponse.Error<Exception>> by lazy {
+        MutableLiveData<ApiResponse.Error<Exception>>()
+    }
+    fun postCardUpload(
+        cardUploadMultipart:RequestBody,
+        recordFile : MultipartBody.Part
+    ){
+        viewModelScope.launch {
+            try {
+                val response = cardRepository.postCardUpload(
+                    cardUploadMultipart = cardUploadMultipart,
+                    recordFile = recordFile
+                )
+                if(response is ApiResponse.Success){
+                    postCardUploadSuccess.postValue(response.data)
+                }else{
+
+                }
+
+            }catch (e:HttpException){
+                postCardUploadFailure.postValue(ApiResponse.Error(e))
+
+            }catch (e:IOException){
+                postCardUploadFailure.postValue(ApiResponse.Error(e))
+
+            }catch (e:Exception){
+                postCardUploadFailure.postValue(ApiResponse.Error(e))
+            }
+        }
+
+
+    }
 
     fun deleteCard(
         cardViewId : Int
