e37b7a408377bc2154598c1068c577f0589107f6
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-04-29 23:59:25 +0900

    fix : 인증 도메인 관련 API에서 인증코드 요청 및 확인 API 응답 클래스 재사용 가능하게 수정

--- Android/moment-android/app/src/main/java/com/capstone/android/application/presentation/AuthViewModel.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/presentation/AuthViewModel.kt
@@ -6,13 +6,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.capstone.android.application.data.remote.auth.AuthRepository
 import com.capstone.android.application.data.remote.auth.auth_code.request.PostAuthAuthCodeRequest
-import com.capstone.android.application.data.remote.auth.auth_code.response.PostAuthAuthCodeResponse
 import com.capstone.android.application.data.remote.auth.auth_code_confirm.request.PatchAuthAuthCodeConfirmRequest
 import com.capstone.android.application.data.remote.auth.change_password.request.PatchAuthChangePasswordRequest
 import com.capstone.android.application.data.remote.auth.login.request.PostAuthLoginRequest
 import com.capstone.android.application.data.remote.auth.login.response.PostAuthLoginResponse
 import com.capstone.android.application.domain.response.ApiResponse
 import com.capstone.android.application.domain.response.MomentResponse
+import com.capstone.android.application.domain.response.auth.AuthResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
@@ -38,8 +38,8 @@ class AuthViewModel @Inject constructor(private val authRepository: AuthReposito
         MutableLiveData<ApiResponse.Error<Exception>>()
     }
 
-    val postAuthAuthCodeSuccess : MutableLiveData<PostAuthAuthCodeResponse> by lazy {
-        MutableLiveData<PostAuthAuthCodeResponse>()
+    val postAuthAuthCodeSuccess : MutableLiveData<AuthResponse> by lazy {
+        MutableLiveData<AuthResponse>()
     }
 
     val postAuthAuthCodeFailure : MutableLiveData<ApiResponse.Error<Exception>> by lazy {
@@ -47,8 +47,8 @@ class AuthViewModel @Inject constructor(private val authRepository: AuthReposito
     }
 
 
-    val patchAuthAuthCodeConfirmSuccess:MutableLiveData<MomentResponse> by lazy {
-        MutableLiveData<MomentResponse>()
+    val patchAuthAuthCodeConfirmSuccess:MutableLiveData<AuthResponse> by lazy {
+        MutableLiveData<AuthResponse>()
     }
     val patchAuthAuthCodeConfirmFailure : MutableLiveData<ApiResponse.Error<Exception>> by lazy {
         MutableLiveData<ApiResponse.Error<Exception>>()
@@ -61,16 +61,17 @@ class AuthViewModel @Inject constructor(private val authRepository: AuthReposito
     ){
         viewModelScope.launch {
             try {
-
                 val response = authRepository.postAuthLogin(
                     body = body
                 )
-                Log.d("weagweg","wegew")
 
 
                 if(response is ApiResponse.Success){
+                    Log.d("waegwgwea","HttpException")
                     postAuthLoginSuccess.value = response.data
                 }else{
+                    Log.d("waegwgwea","HttpException")
+                    postAuthLoginFailure.postValue(ApiResponse.Error(exception = java.lang.Exception()))
 
 //                    val error = (data.body() as MomentNetworkError)
 //                    authFailure.value=MomentNetworkError(
@@ -80,21 +81,28 @@ class AuthViewModel @Inject constructor(private val authRepository: AuthReposito
 
 
             } catch (e: HttpException) {
-                postAuthLoginFailure.postValue(ApiResponse.Error(exception = e))
+                Log.d("waegwgwea","HttpException")
 
                 // Handle specific HTTP error codes
                 when (e.code()) {
-                    404 -> {
-
+                    400 -> {
+                        postAuthLoginFailure.postValue(ApiResponse.Error(exception = e))
                     }
                     // Handle other error codes
                 }
             } catch (e: IOException) {
-                Log.d("awegawegewa","IOE")
+                Log.d("waegwgwea","IOException")
+
+                postAuthLoginFailure.postValue(ApiResponse.Error(exception = e))
+
+
                 // Handle network-related errors
 //                throw NetworkException("Network error occurred", e)
             } catch (e: Exception) {
-                Log.d("awegawegewa","${e.message}")
+                Log.d("waegwgwea","Exception")
+
+                postAuthLoginFailure.postValue(ApiResponse.Error(exception = e))
+
                 // Handle other generic exceptions
             }
         }
