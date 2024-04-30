5651b7891e938119fd617d45264705c1ec267022
Taej-P <asd60asd60@gmail.com>
2024-05-01 00:43:12 +0900

    modify BaseModel for SER task. remove speech mask

--- backend/ai/moment-ai/emotion2vec/iemocap_downstream/model.py
+++ backend/ai/moment-ai/emotion2vec/iemocap_downstream/model.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 
 class BaseModel(nn.Module):
-    def __init__(self, input_dim=768, output_dim=4):
+    def __init__(self, input_dim=768, output_dim=5):
         super().__init__()
         self.pre_net = nn.Linear(input_dim, 256)
 
@@ -11,11 +11,11 @@ class BaseModel(nn.Module):
         self.activate = nn.ReLU()
 
     def forward(self, x, padding_mask=None):
-        x = self.activate(self.pre_net(x))
+        x = self.post_net(self.activate(self.pre_net(x)))
 
-        x = x * (1 - padding_mask.unsqueeze(-1).float())
-        x = x.sum(dim=1) / (1 - padding_mask.float()
-                            ).sum(dim=1, keepdim=True)  # Compute average
+        # x = x * (1 - padding_mask.unsqueeze(-1).float())
+        # x = x.sum(dim=1) / (1 - padding_mask.float()
+        #                     ).sum(dim=1, keepdim=True)  # Compute average
         
-        x = self.post_net(x)
+        # x = self.post_net(x)
         return x
