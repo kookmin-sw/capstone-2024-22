7c1aa88233d661e393badf534a6b690e20a7c658
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-05-02 13:22:01 +0900

    add : open watherd API에서 받아온 데이터를 UI와 통신하기 위한 ViewModel 추가

---
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/presentation/OpenWeatherViewModel.kt
@@ -0,0 +1,55 @@
+package com.capstone.android.application.presentation
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.capstone.android.application.data.remote.open_weather.OpenWeatherRepository
+import com.capstone.android.application.data.remote.open_weather.model.response.GetWeatherResponse
+import com.capstone.android.application.domain.response.ApiResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+@HiltViewModel
+class OpenWeatherViewModel @Inject constructor(private val openWeatherRepository: OpenWeatherRepository): ViewModel() {
+    val getWeatherInCurrentLocationlSuccess : MutableLiveData<GetWeatherResponse> by lazy {
+        MutableLiveData<GetWeatherResponse>()
+    }
+
+    val getWeatherInCurrentLocationFailure : MutableLiveData<ApiResponse.Error<Exception>> by lazy {
+        MutableLiveData<ApiResponse.Error<Exception>>()
+    }
+
+    fun getWeatherInCurrentLocation(
+        lat : String,
+        lon : String,
+        appid : String
+    ){
+        viewModelScope.launch {
+            try {
+                val response = openWeatherRepository.getWeatherInCurrentLocation(
+                    lat = lat,
+                    lon = lon,
+                    appid = appid
+                )
+                Log.d("weagaewgwea",response.toString())
+
+                if(response is ApiResponse.Success){
+                    getWeatherInCurrentLocationlSuccess.postValue(response.data)
+                }
+
+            }catch (e: HttpException){
+                getWeatherInCurrentLocationFailure.postValue(ApiResponse.Error(e))
+
+            }catch (e: IOException){
+                getWeatherInCurrentLocationFailure.postValue(ApiResponse.Error(e))
+
+            }catch (e:Exception){
+                getWeatherInCurrentLocationFailure.postValue(ApiResponse.Error(e))
+            }
+        }
+    }
+}
