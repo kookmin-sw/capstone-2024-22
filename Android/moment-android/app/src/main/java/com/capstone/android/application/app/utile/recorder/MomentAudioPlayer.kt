0b320efa087e8b4830a7b1fb53c254c925f48293
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-05-03 16:26:31 +0900

    refeoctor : 오디오 파일을 실행하는 모듈을 싱글톤으로 구성하기 위해 DI 구성

--- Android/moment-android/app/src/main/java/com/capstone/android/application/app/utile/recorder/MomentAudioPlayer.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/app/utile/recorder/MomentAudioPlayer.kt
@@ -3,10 +3,17 @@ package com.capstone.android.application.app.utile.recorder
 import android.content.Context
 import android.media.MediaPlayer
 import androidx.core.net.toUri
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.components.SingletonComponent
 import java.io.File
+import javax.inject.Inject
 
-class AndroidAudioPlayer(
-    private val context: Context
+@Module
+@InstallIn(SingletonComponent::class)
+class MomentAudioPlayer @Inject constructor(
+    @ActivityContext private val context: Context
 ) : AudioPlayer {
 
     private var player : MediaPlayer?=null
