f36b6f7761c59239b0e62b5da902bfa6ccd2f344
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-05-03 16:35:46 +0900

    refector : 여행파일 조회 API 변경으로 인한 응답 변수 수정

--- Android/moment-android/app/src/main/java/com/capstone/android/application/domain/response/trip_file/TripFile.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/domain/response/trip_file/TripFile.kt
@@ -1,7 +1,7 @@
 package com.capstone.android.application.domain.response.trip_file
 
 data class TripFile(
-    val analyzingCount: Int,
+    val totalCount: Int,
     val email: String,
     val id: Int,
     val tripId: Int,
