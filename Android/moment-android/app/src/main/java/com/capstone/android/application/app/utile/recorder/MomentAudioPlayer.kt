280f1463ea1e29c894efef5a4c503ff81dd164b3
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-05-05 19:03:24 +0900

    refector : 현재 녹음파일의 상태를 알 수 있도록 함수 추가
    - 현재 녹음파일이 play가 되고있는 반환해주는 함수 : isIng()
    - 현재 play가 되고있는 녹음 파일과 play하려는 녹음파일이 같은지 다른지 반환해주느 함수 : checkSameFile()

--- Android/moment-android/app/src/main/java/com/capstone/android/application/app/utile/recorder/MomentAudioPlayer.kt
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/app/utile/recorder/MomentAudioPlayer.kt
@@ -17,16 +17,44 @@ class MomentAudioPlayer @Inject constructor(
 ) : AudioPlayer {
 
     private var player : MediaPlayer?=null
+    private var isIng :Boolean = false
+    private var isCreated:Boolean = false
+    private var fileName:String=""
     override fun playFile(file: File) {
         MediaPlayer.create(context,file.toUri()).apply {
             player = this
             start()
         }
+        isCreated = true
+        isIng = true
+        fileName = file.name
+    }
+
+    fun pause(){
+        player?.pause()
+        isIng = false
+    }
+
+    fun start(){
+        player?.start()
+        isIng = true
+
     }
 
     override fun stop() {
         player?.stop()
         player?.release()
         player = null
+        isIng = false
+        isCreated = false
     }
+
+    fun checkSameFile(fileName:String):Boolean{
+        return fileName == this@MomentAudioPlayer.fileName
+    }
+
+
+
+    fun isIng() = isIng
+
 }
