a8693d4d0931a958927f6fcbe36ff92681bc892c
penguinseohyun <tjgus1036@naver.com>
2024-05-07 18:59:04 +0900

    feat : 영수증 api 모듈

---
+++ Android/moment-android/app/src/main/java/com/capstone/android/application/data/remote/receipt/ReceiptRepositoryInterface.kt
@@ -0,0 +1,28 @@
+package com.capstone.android.application.data.remote.receipt
+
+import com.capstone.android.application.data.remote.receipt.model.receipt_all.getReceiptAllResponse
+import com.capstone.android.application.data.remote.receipt.model.receipt_count.getReceiptCountResponse
+import com.capstone.android.application.data.remote.receipt.model.receipt_delete.deleteReceiptDeleteRequest
+import com.capstone.android.application.data.remote.receipt.model.receipt_post.PostReceiptCreateRequest
+import com.capstone.android.application.domain.response.ApiResponse
+import com.capstone.android.application.domain.response.MomentResponse
+
+interface ReceiptRepositoryInterface {
+
+    suspend fun postReceiptCreate(
+        body : PostReceiptCreateRequest
+    ): ApiResponse<MomentResponse>
+
+    suspend fun deleteReceiptDelete(
+        body: deleteReceiptDeleteRequest
+    ): ApiResponse<MomentResponse>
+
+    suspend fun getReceiptAll(
+        page : Int,
+        size : Int
+    ): ApiResponse<getReceiptAllResponse>
+
+    suspend fun getReceiptCount()
+    : ApiResponse<getReceiptCountResponse>
+
+}
