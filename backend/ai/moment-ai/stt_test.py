5a3af673bff39a552ff58795bf295e9b3a80bec0
Taej-P <asd60asd60@gmail.com>
2024-05-09 17:48:49 +0900

    modify stt test for hallucination test

--- backend/ai/moment-ai/stt_test.py
+++ backend/ai/moment-ai/stt_test.py
@@ -11,7 +11,6 @@ metric = CharErrorRate() # 0.232 : Large-v3 / 0.166 : Large-v2
 
 
 source_csv = "./emotion2vec/datas/AIHUB_for_Whisper.csv"
-data_dir = './emotion2vec/datas/AIHUB/TOTAL'
 source_data = pd.read_csv(source_csv)
 source_data.drop_duplicates()
 stt_model = whisper.load_model("large-v2")
@@ -20,9 +19,8 @@ print(stt_model.device)
 preds = []
 targets = []
 for path, text in tqdm(random.sample(list(zip(source_data['path'], source_data['text'])), 100)):
-  file_path = os.path.join(data_dir, path)
-  if os.path.exists(file_path):
-    res = stt_model.transcribe(os.path.join(data_dir, path), hallucination_silence_threshold=0.5)
+  if os.path.exists(path):
+    res = stt_model.transcribe(path, hallucination_silence_threshold=0.5)
     preds.append(res['text'])
     targets.append(text)
     print(f"\npredict : {res['text']}\nground truth : {text}\ntotal_CER = {metric(preds, targets)}\n")
