756a8f7978ee8fd47a0df834d440dd5ca352514c
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-05-18 00:07:57 +0900

    build : wearable API 사용을 위한 의존성 추가

--- Android/moment-android/app/build.gradle.kts
+++ Android/moment-android/app/build.gradle.kts
@@ -57,6 +57,9 @@ android {
                 "proguard-rules.pro"
             )
         }
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -97,6 +100,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.compose.material3:material3-android:1.2.1")
     implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("com.google.android.gms:play-services-wearable:18.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
